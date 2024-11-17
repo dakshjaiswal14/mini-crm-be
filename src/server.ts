0d5c910266515484a988cbb5c6db6ee41e52f429
dakshjaiswal14 <dakshjaiswal14@gmail.com>
2024-11-17 20:30:55 +0530

    cors error management for frontend integration

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,41 @@
 import express, { Request, Response } from "express";
+import cors from "cors"; // Importing CORS
 import testRoutes from "./routes/testRoutes";
-import "./services/ordersSubscriber"; //importing subscriber-functions
+import "./services/ordersSubscriber"; // Importing subscriber functions
 import "./services/campaignSubscriber";
 import { clerkMiddleware } from "@clerk/express";
 import clerkWebhookHandler from "./services/clerkWebhookHandler";
 
 const app = express();
 const port = 5000;
+
+// CORS Configuration
+const corsOptions = {
+  origin: "*", // Allow all origins
+  methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
+  credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+};
+
+// Enable CORS
+app.use(cors(corsOptions));
+
 // Middleware to parse JSON bodies
 app.use(express.json());
-// https://clerk.com/docs/upgrade-guides/node-to-express
+
+// Clerk Middleware
 app.use(clerkMiddleware());
-// {backendUrl}/api/webhooks is the webhook endpoint
+
+// Webhook endpoint
 app.post("/api/webhooks", async (req: Request, res: Response) => {
   await clerkWebhookHandler(req, res);
 });
-// Start the Redis subscription for new orders
+
 // Simple test route for checking server
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, World!");
 });
 
+// Test Routes
 app.use("/api", testRoutes);
 
 // Start the server
